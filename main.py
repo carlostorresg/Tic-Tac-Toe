a603b5f8c301cdbafb02940b8a8d58d5a12e4493
Carlos Torres <69270612+carlostorresg@users.noreply.github.com>
2024-07-24 00:52:58 -0500

    empty_check errors

---
+++ main.py
@@ -0,0 +1,25 @@
+import random 
+import time 
+import os 
+from functions import displayboard, player_input_marker, flip_coin, clear_screen, timer, empty_check, position_pick 
+
+
+print("---------------Welcome to tic tac toe------------------")
+
+board = [" "] * 10
+
+player1,player2 = player_input_marker()
+
+print("\n")
+
+player = flip_coin(player1,player2)
+
+timer()
+
+clear_screen()
+
+displayboard(board)
+
+#choose position 
+position_pick(board,player)
+
